2c35edd5294967ff20a517e4427aa0250e4d4a42
Giovanni <G-arher@hotmail.com>
2021-09-29 01:39:07 -0600

    message

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
 
 if(navigator.serviceWorker)
 {
-    navigator.serviceWorker.register('/sw.js')
+
+    navigator.serviceWorker.register( (window.location.href.includes('localhost'))?'/sw.js':'/twittor/sw.js');
 }
 
 // Referencias de jQuery
